9d36771616e9bfc9ca0799990b765453bec71234
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-30 01:44:06 +0300

    Scripted auto-commit on change (2020-11-30 01:44:06) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class Main {
-    public static getCounter(Http http , )
+    public static getCounter(Http http , ActorSystem actorSystem , )
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
