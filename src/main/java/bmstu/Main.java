715e34da912cbb8938cf213adcfe63afd9b6e1d1
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-30 01:37:07 +0300

    Scripted auto-commit on change (2020-11-30 01:37:07) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -20,8 +20,7 @@ public class Main {
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
-        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of()
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
