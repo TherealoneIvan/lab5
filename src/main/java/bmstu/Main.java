ae2e473f9ff3dce7a6ff08c2e4891fa9ea2cea0e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-30 03:26:15 +0300

    Scripted auto-commit on change (2020-11-30 03:26:15) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static Flow<HttpRequest, HttpResponse, NotUsed> getCounter(Http http , ActorSystem actorSystem , ActorMaterializer actorMaterializer){
         Flow.of(HttpRequest.class)
                 .map(item -> {
-                    String count = item.getUri()
+                    String count = item.getUri().toString().
                     new Pair<String , Integer> (item.getUri().query() , )
                 })
     }
