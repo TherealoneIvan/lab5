668b82bcf6f1544028f75c7e1ff3f1769de0503b
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-30 04:13:12 +0300

    Scripted auto-commit on change (2020-11-30 04:13:12) by gitwatch.sh

--- src/main/java/bmstu/StroreActor.java
+++ src/main/java/bmstu/StroreActor.java
@@ -6,16 +6,18 @@ import javafx.util.Pair;
 import java.util.Map;
 
 public class StroreActor extends AbstractActor {
-    Map<Pair<String , Integer>, Integer> store;
+    static Map<Pair<String , Integer>, Integer> store;
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
                         Pair.class,
-                        pair ->{
-
-                        }
-                        )
+                        pair -> getRes(pair))
     }
-    private static Boolean
+    private static Pair<Boolean , Integer> getRes(Pair<String , Integer> req){
+        if (store.containsKey(req)){
+            return new Pair<>(true , store.get(req));
+        }else
+            return new Pair<>(false , 0);
+    }
 }
