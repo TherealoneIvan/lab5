7ce8aee05afcbc84c186c3cee39472ae54ed95d0
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-07 16:02:42 +0300

    Scripted auto-commit on change (2020-12-07 16:02:42) by gitwatch.sh

--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -41,7 +41,8 @@ public class Main {
                                 if (item.getKey()){
                                     return  CompletableFuture.completedFuture(item.getValue());
                                 }
-                                Flow<Pair<String, Integer> , Integer , NotUsed> rFlow = 
+                                Flow<Pair<String, Integer> , Integer , NotUsed> rFlow =
+                                        
 
 
                                     }
