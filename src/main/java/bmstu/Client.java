71df6270e032690c2e8f368c1bab886508715170
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-08 01:47:33 +0300

    Scripted auto-commit on change (2020-12-08 01:47:33) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -11,6 +11,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import javafx.util.Pair;
 import org.asynchttpclient.AsyncHttpClient;
 import java.util.ArrayList;
@@ -50,7 +51,8 @@ public class Client {
                                         if (item.getKey()){
                                             return  CompletableFuture.completedFuture(item.getValue());
                                         }
-                                        
+                                Source.from(Collections.singletonList(r))
+                                        .toMat(getSink(), Keep.right()).run(materializer);
 
                             }
     }
