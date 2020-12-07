6a7afe30d034381844030905ed4f54b6213efd1a
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-07 21:28:56 +0300

    Scripted auto-commit on change (2020-12-07 21:28:56) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -1,4 +1,77 @@
 package bmstu;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import javafx.util.Pair;
+import org.asynchttpclient.AsyncHttpClient;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static java.lang.Integer.parseInt;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+
 public class Client {
+    public static final String EMPTY_STRING = "";
+    public static final char EQUALS_CHAR = '=';
+    public static final int TIMEOUT_MILLIS = 5000;
+
+    private static String countBuilder(String uri) {
+        int i = uri.length() - 1;
+        String countOfReq = EMPTY_STRING;
+        while (uri.charAt(i) != EQUALS_CHAR) {
+            countOfReq += uri.charAt(i);
+            i--;
+        }
+        return countOfReq;
+    }
+    public static Flow<HttpRequest, HttpResponse, NotUsed> getCounter(Http http , ActorSystem actorSystem ,
+                                                                      ActorMaterializer actorMaterializer , ActorRef storeActor){
+        Flow.of(HttpRequest.class)
+                .map(item -> {
+                    String uri = item.getUri().toString();
+                    String countOfReq = countBuilder(uri);
+                    return new Pair<String , Integer>(item.getUri().query().toString() ,parseInt(countOfReq));
+                })
+                .mapAsync(
+                        1 , (Pair<String , Integer> pair) -> {
+                            CompletionStage<Object> result = (CompletionStage<Object>) Patterns.ask(storeActor , pair , TIMEOUT_MILLIS);
+                            result.thenCompose( (Pair<Boolean, Integer> item ) ->{
+                                        if (item.getKey()){
+                                            return  CompletableFuture.completedFuture(item.getValue());
+                                        }
+
+                                        Flow<Pair<String, Integer> , Integer , NotUsed> rFlow =
+                                                Flow.<Pair<String , Integer>>create()
+                                                        .mapConcat(
+                                                                requestPair -> {
+                                                                    ArrayList<Pair <String ,Integer>> res = new ArrayList<Pair<String , Integer>>();
+                                                                    for (int i = 0 ; i < requestPair.getValue(); i++ )
+                                                                        res.add(new Pair<String, Integer>(requestPair.getKey() , requestPair.getValue()));
+                                                                    return res;
+                                                                }
+                                                        )
+                                                        .mapAsync( () -> {
+                                                                    AsyncHttpClient asyncHttpClient = asyncHttpClient();
+                                                                    asyncHttpClient
+                                                                            .prepareGet(pair.getKey())
+                                                                            .execute();
+                                                                    return
+                                                                }
+                                                        )
+
+
+                                    }
+                            );
+                        }
+                )
+    }
 }
--- src/main/java/bmstu/Main.java
+++ src/main/java/bmstu/Main.java
@@ -26,64 +26,6 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
 
-    public static final String EMPTY_STRING = "";
-    public static final char EQUALS_CHAR = '=';
-    public static final int TIMEOUT_MILLIS = 5000;
-
-    public static Flow<HttpRequest, HttpResponse, NotUsed> getCounter(Http http , ActorSystem actorSystem ,
-                                                                      ActorMaterializer actorMaterializer , ActorRef storeActor){
-        Flow.of(HttpRequest.class)
-                .map(item -> {
-                    String uri = item.getUri().toString();
-                    String countOfReq = countBuilder(uri);
-                    return new Pair<String , Integer> (item.getUri().query().toString() ,parseInt(countOfReq));
-                })
-                .mapAsync(
-                        1 , (Pair<String , Integer> pair) -> {
-                            CompletionStage<Object> result = (CompletionStage<Object>) Patterns.ask(storeActor , pair , TIMEOUT_MILLIS);
-                            result.thenCompose( (Pair<Boolean, Integer> item ) ->{
-                                if (item.getKey()){
-                                    return  CompletableFuture.completedFuture(item.getValue());
-                                }
-
-                                Flow<Pair<String, Integer> , Integer , NotUsed> rFlow =
-                                        Flow.<Pair<String , Integer>>create()
-                                        .mapConcat(
-                                                requestPair -> {
-                                                   ArrayList<Pair <String ,Integer>> res = new ArrayList<Pair<String , Integer>>();
-                                                   for (int i = 0 ; i < requestPair.getValue(); i++ )
-                                                       res.add(new Pair<String, Integer>(requestPair.getKey() , requestPair.getValue()));
-                                                   return res;
-                                                }
-                                        )
-                                        .mapAsync( () -> {
-                                            AsyncHttpClient asyncHttpClient = asyncHttpClient();
-                                            asyncHttpClient
-                                                    .prepareGet(pair.getKey())
-                                                    .execute();
-                                            return
-                                                }
-                                        )
-
-
-                                    }
-                            );
-                        }
-                )
-    }
-//    private static Future<Object> isInStore(Pair<String , Integer> req , ActorRef storeActor){
-//         return Future<Object> result = Patterns.ask(storeActor , req , TIMEOUT_MILLIS);
-//    };
-    private static String countBuilder(String uri) {
-        int i = uri.length() - 1;
-        String countOfReq = EMPTY_STRING;
-        while (uri.charAt(i) != EQUALS_CHAR){
-            countOfReq += uri.charAt(i);
-            i--;
-        }
-        return countOfReq;
-    }
-
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
