e9cd22b9c5ed12be716e889c0561adf8fea38946
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-10 22:55:25 +0300

    Scripted auto-commit on change (2020-12-10 22:55:25) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -38,7 +38,7 @@ public class Client {
         return Flow.of(HttpRequest.class)
                 .map(item -> {
                     String uri = item.getUri().toString();
-                    String countOfReq = item.getUri().query().getOrElse("count" , )
+                    String countOfReq = item.getUri().query().getOrElse("count" , "");
                     return new Pair<String , Integer>(item.getUri().query().toString() ,parseInt(countOfReq));
                 })
                 .mapAsync(
